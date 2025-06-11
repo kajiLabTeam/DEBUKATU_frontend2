eb9f1ec1e2ccaddb6e9fe4444c79d7718ae3402e
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2025-06-11 17:24:39 +0900

    fix: 表示内容の変更

--- src/features/calorieInput/CalorieInput.tsx
+++ src/features/calorieInput/CalorieInput.tsx
@@ -4,40 +4,34 @@ import { useState, ChangeEvent } from 'react';
 
 export const CalorieInput = () => {
 	const [currentCalorie, setCurrentCalorie] = useState("");//現在の摂取カロリー
-	const [mustCalorie, setMustCalorie] = useState("");//摂取しなければならないカロリー
-	const [eatCount, setEatCount,] = useState("");//食事回数(出力)
+	const [currentWeight, setCurrentWeight] = useState("");
 
+	const onChangeCurrentWeight = (e: ChangeEvent<HTMLInputElement>) => {
+		setCurrentWeight(e.target.value);
+	};
 	const onChangeCurrentCalorie = (e: ChangeEvent<HTMLInputElement>) => {
 		setCurrentCalorie(e.target.value);
 	};
 
-
 	const onClickMustCalorieCal = () => {
-		if (currentCalorie === "") return;
-		console.log(currentCalorie);
-	};
-
-	const onClickEatCount = () => {
+		if (currentWeight === "" || currentCalorie === "") return;
+		console.log({ currentWeight, currentCalorie });
 	};
 
 	return (
 		<div className="calorie_input_area">
 			<h2>カロリー入力画面</h2>
 			<p className="title">カロリー入力画面</p>
+
 			<ul>
-				<span>現在のカロリー摂取量</span>
+				<li>現在の体重</li>
+				<input placeholder="40" value={currentWeight} onChange={onChangeCurrentWeight} />kg
+			</ul>
+			<ul>
+				<li>現在のカロリー摂取量</li>
 				<input placeholder="100" value={currentCalorie} onChange={onChangeCurrentCalorie} />kcal
-				<button onClick={onClickMustCalorieCal}>計算</button>
 			</ul>
-			<ul>
-				<span>足りないカロリー("義務カロリー" - "現在のカロリー摂取量") </span>
-				<span> {mustCalorie} {currentCalorie}</span>kcal
-			</ul>
-			<button onClick={onClickEatCount}>計算</button>
-			<ul>
-				<span>追加の食事回数 </span>
-				<span>{eatCount !== null ? `${eatCount} 回` : ''}</span>
-			</ul>
+			<button onClick={onClickMustCalorieCal}>記録</button>
 		</div>
 	);
 };
--- src/features/weightInput/WeightInput.tsx
+++ src/features/weightInput/WeightInput.tsx
@@ -1,15 +1,11 @@
 import { useState, ChangeEvent } from 'react';
 
 export const WeightInput = () => {
-	const [currentWeight, setCurrentWeight] = useState("");
+
+
 	const [modelWeight, setModelWeight] = useState("");
 	const [lengthOfDays, setLengthOfDays] = useState("");
-	const [mustCalorie, setMustCalorie] = useState("");
 
-
-	const onChangeCurrentWeight = (e: ChangeEvent<HTMLInputElement>) => {
-		setCurrentWeight(e.target.value);
-	};
 
 	const onChangeModelWeight = (e: ChangeEvent<HTMLInputElement>) => {
 		setModelWeight(e.target.value);
@@ -20,13 +16,11 @@ export const WeightInput = () => {
 		setLengthOfDays(e.target.value);
 	};
 
-	const onChangeMustCalorie = (e: ChangeEvent<HTMLInputElement>) => {
-		setMustCalorie(e.target.value);
-	};
+
 	//計算ボタンPush
 	const onClickCalorieCal = () => {
-		if (currentWeight === "" || modelWeight === "" || lengthOfDays === "") return;
-		console.log({ currentWeight, modelWeight, lengthOfDays });
+		if (modelWeight === "" || lengthOfDays === "") return;
+		console.log({ modelWeight, lengthOfDays });
 	};
 
 	return (
@@ -35,23 +29,15 @@ export const WeightInput = () => {
 			<p className="title">体重入力画面</p>
 
 			<ul>
-				<li>現在の体重</li>
-				<input placeholder="40" value={currentWeight} onChange={onChangeCurrentWeight} />kg
-			</ul>
-			<ul>
 				<li>理想の体重 </li>
 				<input placeholder="50" value={modelWeight} onChange={onChangeModelWeight} />kg
 			</ul>
 			<ul>
-				<li>体重増加期間（日）</li>
-				<input placeholder="180" value={lengthOfDays} onChange={onChangeLengthOfDays} />日
+				<li>体重増加期間(月)</li>
+				<input placeholder="1" value={lengthOfDays} onChange={onChangeLengthOfDays} />月
 			</ul>
 
-			<button onClick={onClickCalorieCal}>計算</button>
-			<ul>
-				<span>理想の体重までのカロリー摂取量</span>
-				<span>{mustCalorie} </span>
-			</ul>
+			<button onClick={onClickCalorieCal}>決定</button>
 		</div>
 	);
 };
