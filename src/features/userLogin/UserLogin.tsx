6094628d6124f3bfd3ed8f414b0eac3f47caa956
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2025-06-13 21:50:05 +0900

    Viteに置き換えた

--- src/features/userLogin/UserLogin.tsx
+++ src/features/userLogin/UserLogin.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, useState } from 'react';
-import { postUserInputMock } from '../../api/putUserInput';
 import { useNavigate } from 'react-router';
+import { postUser } from '../../api/putUserInput';
+import { postUserMock } from '../../api/putUserInput';
 
 export const UserLogin = () => {
 	const [userNameText, setUserNameText] = useState("");
@@ -21,7 +22,7 @@ export const UserLogin = () => {
 		setError(null);
 
 		// {"id": 1 }
-		const response = await postUserInputMock(userNameText);
+		const response = await postUser(userNameText);
 		navigate(`/model/${response.user_id}`)
 		console.log(response);
 		setUserNameText("");
