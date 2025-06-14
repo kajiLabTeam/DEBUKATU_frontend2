caeedd31ab2a05333781e89759fb17b005320d74
MihoMiho14134 <153794136+MihoMiho14134@users.noreply.github.com>
2025-06-14 20:59:04 +0900

    fix:エンドポイントの変更

--- viteconfig.ts
+++ viteconfig.ts
@@ -7,7 +7,8 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8090/api/'
+				target: 'http://localhost:8090',
+				changeOrigin: true,
 			}
 		}
 	}
