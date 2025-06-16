7452e2821abcebd585d48681382673859809ee54
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-16 10:50:53 +0900

    add: Weight_Dataを取得する際の型追加

--- src/types/weight.ts
+++ src/types/weight.ts
@@ -1,3 +1,12 @@
 export type PostWeightResponse = {
 	weight_id: number;
 };
+
+
+export type WeightData = {
+	WeightId: number;
+	ModelId: number;
+	UserId: number;
+	CurrentWeight: number;
+	CreatedDate: string;
+};
