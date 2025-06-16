38e51a293d4c228e92796bf0d07fd6d47b0e26c7
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-16 10:50:32 +0900

    add: Model_Dataを取得する際の型追加

--- src/types/model.ts
+++ src/types/model.ts
@@ -1,3 +1,11 @@
 export type PostModelResponse = {
 	model_id: number;
 };
+
+export type ModelData = {
+	ModelId: string;
+	UserId: string;
+	ModelWeight: number;
+	LengthOfDays: number;
+	CreatedDate: string;
+};
