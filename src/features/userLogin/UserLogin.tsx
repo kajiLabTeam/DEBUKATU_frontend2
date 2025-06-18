a89f913d83e2a41670c729bcbeada39e552b7adb
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-18 10:16:22 +0900

    fix: 登録用フォームの移動

--- src/features/userLogin/UserLogin.tsx
+++ src/features/userLogin/UserLogin.tsx
@@ -1,13 +1,12 @@
 import { ChangeEvent, useState } from 'react';
-import { useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 import { postUser } from '../../api/putUserInput';
-import { postUserMock } from '../../api/putUserInput';
 import { getUser } from '../../api/getSpecificUser';
 import { GetUserResponse } from '../../types/user';
 
 export const UserLogin = () => {
 	const [userNameText, setUserNameText] = useState("");
-	const [userIDText, setUserIDText] = useState("");
+	const [userPassText, setUserPassText] = useState("");
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<string | null>(null);
 	const navigate = useNavigate();
@@ -15,33 +14,20 @@ export const UserLogin = () => {
 	const onChangeUserText = (e: ChangeEvent<HTMLInputElement>) => {
 		setUserNameText(e.target.value);
 	};
-	const onChangeUserID = (e: ChangeEvent<HTMLInputElement>) => {
-		setUserIDText(e.target.value);
+	const onChangeUserPass = (e: ChangeEvent<HTMLInputElement>) => {
+		setUserPassText(e.target.value);
 	};
 
-	//入力ボタン押したら
-	const onClickInput = async () => {
-		if (userNameText === "") return;
-		setUserNameText(userNameText);
-		// --- API通信処理
-		setLoading(true);
-		setError(null);
-
-		// {"id": 1 }
-		const response = await postUser(userNameText);
-		navigate(`/model/${response.user_id}`)
-		console.log(response);
-		setUserNameText("");
-	};
 
 	const onClickLogin = async () => {
-		if (userIDText === "") return;
-		setUserIDText(userIDText);
+		if (userNameText === "" || userPassText === "") return;
+		setUserNameText(userNameText);
+		setUserPassText(userPassText);
 		// --- API通信処理
 		setLoading(true);
 		setError(null);
 
-		const response: GetUserResponse = await getUser(userIDText);
+		const response: GetUserResponse = await getUser(userNameText, userPassText);
 		if (!response || Object.keys(response).length === 0) {
 			setError("ユーザ認証エラー");
 			setLoading(false);
@@ -49,19 +35,18 @@ export const UserLogin = () => {
 		}
 		navigate(`/home/${String(response.UserId)}`)
 		console.log(response);
-		setUserIDText("");
+		setUserNameText("");
 	}
 
 	return (
 		<div >
-			<h2> ユーザログイン画面 </h2>
-			<p className="title">ユーザ登録</p>
+			<div><Link to={`/users/input`}>ユーザ登録へ</Link></div>
+			<h2> ユーザログイン </h2>
+			<p className="title">ユーザ名</p>
 			<input placeholder="ユーザ名を入力" value={userNameText} onChange={onChangeUserText} />
-			<button onClick={onClickInput}>入力</button>
-
-			<p className="title">ユーザログイン</p>
-			<input placeholder="ユーザIDを入力" value={userIDText} onChange={onChangeUserID} />
-			<button onClick={onClickLogin}>入力</button>
+			<p className="title">パスワード</p>
+			<input placeholder="パスワードを入力" value={userPassText} onChange={onChangeUserPass} />
+			<button onClick={onClickLogin}>ログイン</button>
 		</div >
 	)
 }
